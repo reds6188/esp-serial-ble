c7f5081a7930f39c37c83d4a0875e0610dec8ebb
reds6188 <reds6188@gmail.com>
2025-01-03 15:05:31 +0100

    Completed SerialBle class implementation

--- include/serial-ble.h
+++ include/serial-ble.h
@@ -9,6 +9,8 @@
 
 #define BLE_CALLBACK NimBLECharacteristicCallbacks
 
+#define BLE_TX_MAX_SIZE     320
+
 // List of Service and Characteristic UUIDs
 #define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define RX_DATA_UUID "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -16,23 +18,36 @@
 
 class SerialBle {
     private:
-        void (*cbOnReceive)(const char *data, int data_size);
+        uint8_t _btMacAddr[6] = {0};
+        char _btNameStr[14] = "";
+        bool _ble_enabled = false;
+        NimBLEServer *_pServer;
+        NimBLECharacteristic *_pTxCharacteristic;
+        NimBLEService *_pService;
+        String genDeviceName(uint8_t *address);
     public:
         SerialBle();
-        void init(const char * name);
+        void getBtMacAddress(uint8_t *address);
+        static String btMAcAddress_toString(const uint8_t *address);
+        void printBLEinfo(void);
+        void init(const char * name, void (*cbOnReceive)(const char *data, int data_size));
+        void deinitBLE(void);
+        void sendData(uint8_t *data, int size);
+        bool isEnabled(void);
 };
 
 class bleRxHandler : public NimBLECharacteristicCallbacks {
+private:
+    void (*cbOnReceive)(const char *data, int data_size);
 public:
+    bleRxHandler(void (*callback)(const char *data, int data_size));
     void onWrite(NimBLECharacteristic *pCharacteristic);
 };
 
-void getBtMacAddress(uint8_t *address);
-String btMAcAddress_toString(const uint8_t *address);
-void printBLEinfo(void);
-void initBLE(const char *name, void (*callback)(const char *data, int data_size));
-void deinitBLE(void);
-void sendDataNotify(uint8_t *data, uint8_t size);
-bool isEnabledBLE(void);
+class ServerCallbacks: public NimBLEServerCallbacks {
+public:
+	void onConnect(NimBLEServer *pServer, ble_gap_conn_desc *desc);
+	void onDisconnect(NimBLEServer *pServer);
+};
 
 #endif /* SERIAL_BLE_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,53 @@
 #include <Arduino.h>
 #include "../include/serial-ble.h"
 
-void ble_callback(const char *data, int data_size) {
+#define MAIN_T	"MAIN"
 
+#define BLE_RESTART "restart"
+#define BLE_HELP    "help"
+#define BLE_LED     "led"
+
+SerialBle ble;
+
+void ble_callback(const char *data, int data_size) {
+    String res;
+	if(data_size < 96) {
+		if(!strncmp(data, BLE_HELP, sizeof(BLE_HELP)-1)) {
+			String info_str[9];
+			info_str[0] = "=========== COMMAND LIST ===========";
+			info_str[1] = "'info' ------> Get device info";
+			info_str[2] = "'pwd=xyz' ---> Set Wi-Fi password with string \"xyz\"";
+			info_str[3] = "'reset' -----> Reset Wi-Fi SSID and password";
+			info_str[4] = "'restart' ---> Reset device";
+			info_str[5] = "'save' ------> Save Wi-Fi SSID and password";
+			info_str[6] = "'scan' ------> Scan Wi-Fi networks";
+			info_str[7] = "'set=n' -----> Set network 'n' of the scan list";
+			info_str[8] = "'ssid=xyz' --> Set Wi-Fi SSID with string \"xyz\"";
+			for(uint8_t i=0 ; i<9 ; i++) {
+				info_str[i] += "\r\n";
+				ble.sendData((uint8_t *)info_str[i].c_str(), info_str[i].length());
+			}
+		}
+		else if(!strncmp(data, BLE_RESTART, sizeof(BLE_RESTART)-1)) {
+			res = "OK >>> Restarting device, please reconnect after few seconds\r\n";
+			ble.sendData((uint8_t *)res.c_str(), res.length());
+			ESP.restart();
+		}
+		else {
+			console.warning(MAIN_T, "Unknown string");
+			res = "ERR >>> Invalid command\r\n";
+			ble.sendData((uint8_t *)res.c_str(), res.length());
+		}
+	}
+	else {
+		console.error(MAIN_T, "Buffer exceed length");
+		res = "ERR >>> Message is too long\r\n";
+		ble.sendData((uint8_t *)res.c_str(), res.length());
+	}
 }
 
 void setup () {
-    initBLE("prova", ble_callback);
+    ble.init("prova", ble_callback);
 }
 
 void loop() {
